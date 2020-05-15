246a1f82be523e9c0e9f8c463b471ea3f61ddc35
23_bott <59711850+bearbotics6391@users.noreply.github.com>
2020-05-15 15:32:44 -0500

    Add files via upload

--- gallery.js
+++ gallery.js
@@ -146,3 +146,43 @@ function showSlides3(n) {
   dots3[slideIndex3-1].className += " active3";
   captionText.innerHTML = dots3[slideIndex3-1].alt;
 }
+
+// Open the Modal
+function openModal4() {
+  document.getElementById("myModal4").style.display = "block";
+}
+
+// Close the Modal
+function closeModal4() {
+  document.getElementById("myModal4").style.display = "none";
+}
+
+var slideIndex4 = 1;
+showSlides4(slideIndex4);
+
+// Next/previous controls
+function plusSlides4(n) {
+  showSlides4(slideIndex4 += n);
+}
+
+// Thumbnail image controls
+function currentSlide4(n) {
+  showSlides4(slideIndex4 = n);
+}
+
+function showSlides4(n) {
+  var i;
+  var slides4 = document.getElementsByClassName("mySlides4");
+  var dots4 = document.getElementsByClassName("demo4");
+  if (n > slides4.length) {slideIndex4 = 1}
+  if (n < 1) {slideIndex4 = slides4.length}
+  for (i = 0; i < slides4.length; i++) {
+    slides4[i].style.display = "none";
+  }
+  for (i = 0; i < dots4.length; i++) {
+    dots4[i].className = dots4[i].className.replace(" active4", "");
+  }
+  slides4[slideIndex4-1].style.display = "block";
+  dots4[slideIndex4-1].className += " active4";
+  captionText.innerHTML = dots4[slideIndex4-1].alt;
+}
